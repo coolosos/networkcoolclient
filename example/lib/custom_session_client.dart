b97a2a191ad7ba12a9b9de8619f649b3e482fcf8
Cayetano Bañón <cayetano.banon@gmail.com>
2025-05-26 19:41:48 +0200

    Added readme and example folder

---
+++ example/lib/custom_session_client.dart
@@ -0,0 +1,45 @@
+import 'package:network_cool_client/network_cool_client.dart';
+
+import 'observer/custom_network_observer.dart';
+
+final class MySessionClient extends SessionClient {
+  MySessionClient()
+      : super(
+          id: 'my-session-client',
+          client: Client(),
+        );
+
+  @override
+  Future<String?> getBearerToken() async {
+    // Replace with secure storage or other mechanism
+    return 'example-bearer-token';
+  }
+
+  @override
+  Future<bool> renewSession() async {
+    // Implement your session renewal logic here
+    return true;
+  }
+}
+
+void main() async {
+  final client = MySessionClient()
+    ..addObserver(
+      CustomNetworkObserver(
+        onNotLoggedIn: () async {
+          print('Not logged in');
+        },
+        onOffline: () async {
+          print('offline');
+        },
+        onUndermantenance: () async {
+          print('onUndermantenance');
+        },
+      ),
+    );
+
+  final response = await client
+      .get(Uri.parse('https://jsonplaceholder.typicode.com/posts/1'));
+
+  print(response.body);
+}
---
+++ example/lib/observer/custom_network_observer.dart
@@ -0,0 +1,64 @@
+import 'package:network_cool_client/network_cool_client.dart';
+
+class CustomNetworkObserver implements NetworkObserver {
+  CustomNetworkObserver({
+    this.onUndermantenance,
+    this.onOffline,
+    this.onNotLoggedIn,
+  });
+
+  final Future<void> Function()? onUndermantenance;
+  final Future<void> Function()? onOffline;
+  final Future<void> Function()? onNotLoggedIn;
+
+  @override
+  void onChangeNetworkStatus({
+    required NetworkState previousState,
+    required NetworkState currentState,
+    required BaseClient client,
+  }) {
+    if ((client.runtimeType == HttpClient) || (previousState == currentState)) {
+      return;
+    }
+
+    currentState.resolve(
+      onUnderMaintenance: () {
+        if (onUndermantenance case final onUndermantenance?) {
+          onUndermantenance();
+        }
+      },
+      onOffline: () {
+        if (onOffline case final onOffline?) {
+          onOffline();
+        }
+      },
+      onNotLoggedIn: () {
+        if (onNotLoggedIn case final onNotLoggedIn?) {
+          onNotLoggedIn();
+        }
+      },
+      onNormal: () {},
+      onReLogged: () {},
+    );
+  }
+
+  @override
+  void onError({
+    required Object error,
+    required StackTrace? stackTrace,
+    required BaseClient client,
+  }) {
+    print('Error handle');
+  }
+
+  @override
+  void onResponse({
+    required OnResponse response,
+    required BaseClient client,
+  }) {
+    print(response.body);
+  }
+
+  @override
+  void dispose() {}
+}
